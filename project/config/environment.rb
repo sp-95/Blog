2ceeecfb85925bba4238f19b18fca4c822e11068
subas48 <subas_sht@hotmail.com>
2016-06-21 10:34:54 +0545

    Forgot password

--- project/config/environment.rb
+++ project/config/environment.rb
@@ -1,5 +1,17 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
+require File.expand_path('../application', FILE)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => 587,
+    :authentication => :login,
+    :user_name      => "subas48@gmail.com",
+    :password       => "wenesdaygooglemail",
+    :enable_starttls_auto => true
+}
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.default charset:"utf-8"
